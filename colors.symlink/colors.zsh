fbad4df8120b1c80ec7a73a514c04d1183668c81
Jack Halford <jackhalford7@gmail.com>
2016-03-13 02:10:38 +0100

    colors, tmux and zshrc fix

--- colors.symlink/colors.zsh
+++ colors.symlink/colors.zsh
@@ -1,12 +1,16 @@
 # COLORS CONFIG
 
+echo -n "Making color shortcuts..."
 if [ -n "PS1" ]; then #if statement guards adding these helpers for non interactive shells
 	eval "$(~/.colors/base16-shell/profile_helper.sh)"
 fi
+echo " Done"
 
 theme=monokai
 variant=dark
 
-echo -n 'Setting theme: base16_'"$theme"'_'"$variant"'...'
+echo -n 'Setting colorscheme: base16_'"$theme"'_'"$variant"'...'
 base16_"$theme"_"$variant"
 echo " Done"
+
+alias colortest="./dotfiles/colors.symlink/base16-shell/colortest"
