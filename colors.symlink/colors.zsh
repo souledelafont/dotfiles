f9643846cc2507b7dbdea30525faea1b62132133
Jack Halford <jackhalford7@gmail.com>
2016-04-16 13:13:36 +0200

    ansi escapes for colors must be in echo

--- colors.symlink/colors.zsh
+++ colors.symlink/colors.zsh
@@ -1,11 +1,11 @@
 # COLORS CONFIG
 
-echo -n "Loading color shortcuts..."
 if [ -n "PS1" ]; then #if statement guards adding these helpers for non interactive shells
+	echo -n "Loading color shortcuts..."
 	eval "$(~/.colors/base16-shell/profile_helper.sh)"
+	# loadconf "color helpers" '"$(~/.colors/base16-shell/profile_helper.sh)"'
 fi
 echo $success" Done"$normal
-# loadconf "color helpers" 'if [ -n "PS1" ]; eval "$(~/.colors/base16-shell/profile_helper.sh)";fi'
 
 theme=google
 variant=dark
--- tmux.symlink/tmux.zsh
+++ tmux.symlink/tmux.zsh
@@ -1,12 +1,13 @@
 # Tmux sessions
-
 if tmux ls 2>/dev/null 1>&2; then
 	loadconf "tmux conf" "tmux source ~/.tmux.conf 1>/dev/null"
 
+echo $info
 if [[ -z $TMUX ]]; then # if not in tmux show sessions
 	echo "Currently running Tmux sessions:"
-	$info tmux ls$normal
+	tmux ls
 fi
+echo $normal
 else
 	echo "No currently running Tmux sessions !"
 fi
