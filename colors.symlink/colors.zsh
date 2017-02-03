e66e2725bb00793194fe884bb09b25c576d58058
Jack Halford <jack@crans.org>
2017-02-03 22:56:01 +0100

    some pimp for zsh loadup, gotta work on making it faster next

--- colors.symlink/colors.zsh
+++ colors.symlink/colors.zsh
@@ -1,15 +1,8 @@
 # COLORS CONFIG
 
 if [ -n "$PS1" ]; then #if statement guards adding these helpers for non interactive shells
-	printf "Loading color shortcuts..."
-	eval "$(~/.colors/base16-shell/profile_helper.sh)"
-	printf $success" Done"$normal'\n'
-	# loadconf "color shortcuts" '"$(~/.colors/base16-shell/profile_helper.sh)"'
+	loadconf "color shortcuts" "$(~/.colors/base16-shell/profile_helper.sh)"
+	loadconf "colorscheme" "source ~/.base16_theme"
 fi
-
-
-printf 'Setting colorscheme'
-source ~/.base16_theme
-printf $success" Done"$normal'\n'
 
 alias colortest="~/dotfiles/colors.symlink/base16-shell/colortest"
--- tmux.symlink/tmux.zsh
+++ tmux.symlink/tmux.zsh
@@ -2,12 +2,12 @@
 tmux attach -t base 2>/dev/null || tmux new -s base 2>/dev/null
 if tmux ls 2>/dev/null 1>&2; then
 	loadconf "tmux conf" "tmux source-file ~/.tmux.conf 1>/dev/null"
-printf $info
-if [[ -z $TMUX ]]; then # if not in tmux show sessions
-	echo "Currently running Tmux sessions:"
-	tmux ls
-fi
-printf $normal
+	if [[ -z $TMUX ]]; then # if not in tmux show sessions
+		printf $color_info
+		echo "Currently running Tmux sessions:"
+		tmux ls
+		printf $color_default
+	fi
 else
 	echo "No currently running Tmux sessions !"
 fi
