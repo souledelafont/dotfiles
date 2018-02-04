f85e33abee196d0d847a44f5bba683d043d806fb
Jack Halford <jack@crans.org>
2018-02-04 15:20:36 +0100

    docker stuff

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -1,9 +1,11 @@
 alias d="docker"
 alias dm="docker-machine"
 
-# store on hard drive
-# export MACHINE_STORAGE_PATH="/Volumes/Albert"
-# eval $(docker-machine env default)
+drun () {
+	export MACHINE_STORAGE_PATH="/tmp"
+	dm create default
+	eval $(dm env default)
+}
 
 docker-run-ubuntu () {
 	port=12345
